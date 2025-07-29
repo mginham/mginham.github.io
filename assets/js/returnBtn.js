415ee724f6423f69677cb746afc00db42d6031d4
Megan Ginham <mginham@uwo.ca>
2025-07-28 19:29:25 -0600

    Created a vite + react app with tailwind

--- assets/js/returnBtn.js
+++
@@ -1,7 +0,0 @@
-// Redirects back to the home page when the return btn is clicked
-
-const button = document.getElementById("return_btn");
-
-button.addEventListener("click", function() {
-    window.location.href = "index.html";
-});
